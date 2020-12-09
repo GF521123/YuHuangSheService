3428c53a90afb0476b5ed3073345dbf858552e87
2278417331@qq.com <Gwf521123@>
2020-12-09 18:16:34 +0800

    login

--- src/main/java/com/gwf/yuhuangshe/YuhuangsheApplication.java
+++ src/main/java/com/gwf/yuhuangshe/YuhuangsheApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.gwf.yuhuangshe.dao")//使用MapperScan批量扫描所有的Mapper接口；
+    @MapperScan("com.gwf.yuhuangshe.dao")//使用MapperScan批量扫描所有的Mapper接口；
 public class YuhuangsheApplication {
 
     public static void main(String[] args) {
