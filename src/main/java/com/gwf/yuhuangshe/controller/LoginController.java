5a222a66d528e07471641182047194f9de67cdf5
2278417331@qq.com <Gwf521123@>
2020-12-09 17:08:10 +0800

    login html->service

--- src/main/java/com/gwf/yuhuangshe/controller/LoginController.java
+++ src/main/java/com/gwf/yuhuangshe/controller/LoginController.java
@@ -13,7 +13,8 @@ public class LoginController {
     private LoginService loginService;
     @RequestMapping("/login")
     public void login(User user){
-//        loginService.login(user);
-        System.out.println(user);
+        loginService.login(user);
+//        System.out.println(user.getUName());
+//        System.out.println(user.getUPasswd());
     }
 }
--- src/main/java/com/gwf/yuhuangshe/entity/User.java
+++ src/main/java/com/gwf/yuhuangshe/entity/User.java
@@ -10,6 +10,45 @@ public class User {
 //    private String UCreateDate;
     private String UIphone;
 
+    public User() {
+    }
 
+    public User(int UId, String UName, String UPasswd, String UIphone) {
+        this.UId = UId;
+        this.UName = UName;
+        this.UPasswd = UPasswd;
+        this.UIphone = UIphone;
+    }
 
+    public int getUId() {
+        return UId;
+    }
+
+    public void setUId(int UId) {
+        this.UId = UId;
+    }
+
+    public String getUName() {
+        return UName;
+    }
+
+    public void setUName(String UName) {
+        this.UName = UName;
+    }
+
+    public String getUPasswd() {
+        return UPasswd;
+    }
+
+    public void setUPasswd(String UPasswd) {
+        this.UPasswd = UPasswd;
+    }
+
+    public String getUIphone() {
+        return UIphone;
+    }
+
+    public void setUIphone(String UIphone) {
+        this.UIphone = UIphone;
+    }
 }
