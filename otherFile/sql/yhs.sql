e89dab9fcd74fc94c0ee4df553a9db4bb69d2b90
2278417331@qq.com <Gwf521123@>
2020-12-11 17:08:52 +0800

    menu 菜单

--- otherFile/sql/yhs.sql
+++ otherFile/sql/yhs.sql
@@ -11,11 +11,30 @@
  Target Server Version : 50527
  File Encoding         : 65001
 
- Date: 09/12/2020 18:06:21
+ Date: 11/12/2020 17:03:25
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for u_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `u_menu`;
+CREATE TABLE `u_menu`  (
+  `MId` int(11) NOT NULL,
+  `MTitle` varchar(255) CHARACTER SET gb2312 COLLATE gb2312_bin NULL DEFAULT NULL COMMENT '标签，显示',
+  `MValue` varchar(255) CHARACTER SET gb2312 COLLATE gb2312_bin NULL DEFAULT NULL COMMENT '菜单名称，关联',
+  `MSort` int(255) NULL DEFAULT NULL COMMENT '排序用',
+  `MUrl` varchar(255) CHARACTER SET gb2312 COLLATE gb2312_bin NULL DEFAULT NULL COMMENT 'url路径',
+  `MOther` varchar(255) CHARACTER SET gb2312 COLLATE gb2312_bin NULL DEFAULT NULL COMMENT '字段备用\r\n',
+  PRIMARY KEY (`MId`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = gb2312 COLLATE = gb2312_bin ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of u_menu
+-- ----------------------------
+INSERT INTO `u_menu` VALUES (1, '测试', '测试', 50, 'home.html', NULL);
 
 -- ----------------------------
 -- Table structure for u_operators
@@ -27,6 +46,11 @@ CREATE TABLE `u_operators`  (
   `UPassword` varchar(100) CHARACTER SET gb2312 COLLATE gb2312_bin NULL DEFAULT NULL,
   `UIphone` varchar(100) CHARACTER SET gb2312 COLLATE gb2312_bin NULL DEFAULT NULL,
   PRIMARY KEY (`UId`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = gb2312 COLLATE = gb2312_bin ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = gb2312 COLLATE = gb2312_bin ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of u_operators
+-- ----------------------------
+INSERT INTO `u_operators` VALUES (1, '123', '123', '123');
 
 SET FOREIGN_KEY_CHECKS = 1;
