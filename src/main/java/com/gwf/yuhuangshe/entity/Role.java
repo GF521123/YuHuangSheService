2508bd5a9458501b30b3f98641fd08269c4f49bc
2278417331@qq.com <Gwf521123@>
2020-12-24 17:44:27 +0800

    role角色菜单

---
+++ src/main/java/com/gwf/yuhuangshe/entity/Role.java
@@ -0,0 +1,47 @@
+package com.gwf.yuhuangshe.entity;
+
+
+public class Role {
+
+  private Integer id;//编号
+  private String rname;//校色
+  private String title;//说明
+  private int state;//状态：0不启用(默认)，1启用
+
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+
+  public String getRname() {
+    return rname;
+  }
+
+  public void setRname(String rname) {
+    this.rname = rname;
+  }
+
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+
+  public int getState() {
+    return state;
+  }
+
+  public void setState(int state) {
+    this.state = state;
+  }
+
+}
